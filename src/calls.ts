4647e5c54c12d05b7a7848c83c1a406663fef6fb
Tim Hornback <tahornback@gmail.com>
2025-03-15 02:08:22 +0000

    browser VSCode on mobile is torture

--- src/calls.ts
+++ src/calls.ts
@@ -2,9 +2,9 @@ import type { Maybe } from "./types"
 
 export const getLatest = async (id: Maybe<string>) => {
     //todo
-    const r = await fetch("https://prices.runescape.wiki/api/v1/osrs/latest", {
-        
-    })
+    const params = new URLSearchParams()
+    if(id) params.append("id",id)
+    const r = await fetch(`https://prices.runescape.wiki/api/v1/osrs/latest?${params}`)
     return await r.json()
 }
 
